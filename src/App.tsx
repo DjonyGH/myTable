acaab5df0d3f26d783baf2a3e8dfc63c0db8e7f1
Evgeniy Kalimullin <counsil@mail.ru>
2023-01-04 20:49:46 +0700

    added styles props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { data2 } from './data/data2'
 import { data3 } from './data/data3'
 import { data4 } from './data/data4'
 import { MyTable } from './MyTable/MyTable'
-import { IColumn } from './types'
+import { IColumn, TValueRowSpanObject } from './types'
 
 function App() {
   const columns: IColumn[] = [
@@ -42,12 +42,43 @@ function App() {
     },
   ]
 
+  const tableStyle: React.CSSProperties = {
+    borderCollapse: 'collapse',
+  }
+
+  const thStyle: React.CSSProperties = {
+    textAlign: 'left',
+    padding: '8px 16px',
+    height: '63px',
+    border: '1px solid hsl(220deg, 6%, 80%)',
+  }
+
+  const tdStyle: React.CSSProperties = {
+    textAlign: 'left',
+    padding: '8px 16px',
+    height: '32px',
+    border: '1px solid hsl(220deg, 6%, 80%)',
+  }
+
+  const rowStylePrepare = (row: TValueRowSpanObject) => {
+    if (row.id.value === 1) {
+      return { backgroundColor: '#aeb1b7' }
+    }
+  }
+
   return (
     <div className='App'>
       {/* <h1>Таблица 1</h1>
       <MyTable rows={data1} /> */}
       <h1>Таблица 2</h1>
-      <MyTable columns={columns} rows={data2} />
+      <MyTable
+        columns={columns}
+        rows={data2}
+        tableStyle={tableStyle}
+        thStyle={thStyle}
+        tdStyle={tdStyle}
+        rowStylePrepare={rowStylePrepare}
+      />
       {/* <h1>Таблица 3</h1>
       <MyTable rows={data3} />
       <h1>Таблица 4</h1>
--- src/MyTable/MyTable.tsx
+++ src/MyTable/MyTable.tsx
@@ -7,45 +7,57 @@ interface IProps {
   rows: TRow[]
   columns?: IColumn[]
   rowHeight?: number
+  tableStyle?: React.CSSProperties
+  thStyle?: React.CSSProperties
+  tdStyle?: React.CSSProperties
+  rowStylePrepare?: (row: TValueRowSpanObject, rowIndex?: number) => React.CSSProperties | undefined
 }
 
-export const MyTable: React.FC<IProps> = ({ columns, rows, rowHeight }) => {
+export const MyTable: React.FC<IProps> = ({
+  columns,
+  rows,
+  rowHeight,
+  tableStyle,
+  thStyle,
+  tdStyle,
+  rowStylePrepare,
+}) => {
   const preparedRows = prepareRows(rows)
 
   return (
-    <table className={styles.table}>
+    <table style={tableStyle}>
       <thead>
         <tr>
           {columns &&
             columns.map((col, idx) => (
-              <td className={styles.cell} style={{ width: `${col.width}px` }} key={idx}>
+              <th style={{ ...thStyle, width: `${col.width}px` }} key={idx}>
                 {col.title}
-              </td>
+              </th>
             ))}
         </tr>
       </thead>
 
       <tbody>
         {preparedRows.map((row: TValueRowSpanObject, idx: number) => (
-          <tr key={idx} style={{ height: rowHeight ? `${rowHeight}px` : 'auto' }}>
+          <tr key={idx} style={{ ...rowStylePrepare?.(row), height: rowHeight ? `${rowHeight}px` : 'auto' }}>
             {columns
               ? columns.map((col, idx) => {
                   if (col.name in row) {
                     return (
-                      <td rowSpan={row[col.name]?.rowSpan} className={styles.cell} key={idx}>
+                      <td rowSpan={row[col.name]?.rowSpan} style={{ ...tdStyle }} key={idx}>
                         {col.cellRender?.(row[col.name]?.value, row) || row[col.name]?.value}
                       </td>
                     )
                   } else {
                     return (
-                      <td className={styles.cell} key={idx}>
+                      <td style={{ ...tdStyle }} key={idx}>
                         <i>No data</i>
                       </td>
                     )
                   }
                 })
               : (Object.keys(row) as (keyof typeof row)[]).map((fieldKey, idx) => (
-                  <td rowSpan={row[fieldKey]?.rowSpan} className={styles.cell} key={idx}>
+                  <td rowSpan={row[fieldKey]?.rowSpan} style={{ ...tdStyle }} key={idx}>
                     {row[fieldKey]?.value}
                   </td>
                 ))}
